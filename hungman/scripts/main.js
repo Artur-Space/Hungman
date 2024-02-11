2dd22b2f56434ebfc05b2f19fda5681b77ef57e3
Artur-Space <argubriy@gmail.com>
2024-02-11 08:42:05 +0200

    feat: add addEventListener for letters(2024-02-11 08:42:05)

--- hungman/scripts/main.js
+++ hungman/scripts/main.js
@@ -3,6 +3,7 @@ const keyboardDiv = document.querySelector(".keyboard");
 
 const getRandomWord = () => {
   const { word, hint } = wordList[Math.floor(Math.random() * wordList.length)];
+  currentWord = word;
   console.log(word);
   document.querySelector(".hint-text b").innerText = hint;
   wordDisplay.innerHTML = word
@@ -11,11 +12,22 @@ const getRandomWord = () => {
     .join("");
 };
 
+const initGame = (button, clickedLetter) => {
+  if (currentWord.includes(clickedLetter)) {
+    console.log(clickedLetter, " is exist on the word");
+  } else {
+    console.log(clickedLetter, " is not exist on the word");
+  }
+};
+
 // keyboardButtons
 for (let i = 97; i <= 122; i++) {
   const button = document.createElement("button");
   button.innerText = String.fromCharCode(i);
   keyboardDiv.appendChild(button);
+  button.addEventListener("click", (e) =>
+    initGame(e.target, String.fromCharCode(i))
+  );
 }
 
 getRandomWord();
